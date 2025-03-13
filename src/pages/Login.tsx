07116b8647826a737f886cc501641be1ee41fc7b
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2025-03-13 13:09:45 +0700

    fix: bug http

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -8,7 +8,7 @@ export default function Login() {
         <CardHeader className="text-center">
           <div className="flex justify-center">
             {/* <svg
-              xmlns="http://www.w3.org/2000/svg"
+              xmlns="https://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
